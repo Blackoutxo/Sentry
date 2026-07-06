2cb1a18772f1effedca0a3868c2ae90e881a7697
Blackout180 <Semperx123@gmail.com>
2026-07-06 21:10:37 +0545

    Okay, From now on I will try to add a new GUI style for this kind of desktop tool application, removed the previous GUI layout.

--- src/main/java/me/blackout/Sentry/Panels.java
+++ src/main/java/me/blackout/Sentry/Panels.java
@@ -4,35 +4,26 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Panels {
+
     public static void passkeysPanel(JFrame frame, String fileData) {
         JPanel panel = new JPanel(new BorderLayout(10, 10));
-        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JPanel bottomPanel = new JPanel();
 
         JLabel title = new JLabel("PASS KEYS", SwingConstants.CENTER);
         title.setFont(Utils.spaceGrotesk.deriveFont(Font.BOLD, 25f));
 
-        JTextArea dataArea = new JTextArea(fileData);
-        dataArea.setEditable(false);
-        dataArea.setLineWrap(true);
-        dataArea.setWrapStyleWord(true);
-        dataArea.setSize(200, 100);
+        JButton addKey = new JButton("Add new key");
 
-        JScrollPane scrollPane = new JScrollPane(dataArea);
+        addKey.addActionListener(e -> {
+            JFrame addKeyFrame = new JFrame();
 
-        JButton keyButton = new JButton("Add new keys");
-        JButton edit = new JButton("Edit");
-
-        bottomPanel.add(keyButton);
-        bottomPanel.add(edit);
-
-        edit.addActionListener(e -> {
-            dataArea.setEditable(true);
-            edit.setLabel("Done");
+            addKeyFrame.setSize(200, 200);
         });
 
-        panel.add(title, BorderLayout.NORTH);
-        panel.add(scrollPane, BorderLayout.WEST);
-        panel.add(bottomPanel, BorderLayout.SOUTH);
+        panel.add(title);
+        panel.add(bottomPanel);
+
+        bottomPanel.add(addKey);
 
         frame.getContentPane().removeAll();
         frame.add(panel);
